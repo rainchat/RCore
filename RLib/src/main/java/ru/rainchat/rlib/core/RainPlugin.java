d66632bffea8fe751098f12bc7119961060ce02b
Павел Сидоренко <super.pasha4444@yandex.ru>
2024-09-16 23:06:04 +1000

    2.1 lib loader test

--- RLib/src/main/java/ru/rainchat/rlib/core/RainPlugin.java
+++ RLib/src/main/java/ru/rainchat/rlib/core/RainPlugin.java
@@ -1,11 +1,14 @@
 package ru.rainchat.rlib.core;
 
 import ru.rainchat.rlib.commands.CommandController;
+import ru.rainchat.rlib.core.libraries.LibraryLoader;
 import ru.rainchat.rlib.utils.RUtility;
 import ru.rainchat.rlib.utils.config.yaml.YamlConfig;
 import ru.rainchat.rlib.utils.injection.Injector;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public abstract class RainPlugin extends JavaPlugin {
 
@@ -15,6 +18,21 @@ public abstract class RainPlugin extends JavaPlugin {
     protected abstract void enable();
 
     protected abstract void disable();
+
+    @Override
+    public void onLoad() {
+        LibraryLoader loader = new LibraryLoader(this, new File(getDataFolder(), "libraries"));
+
+        loader.loadLibrary("com.github.rainchat.rcore", "EventManager", "2.1", "https://jitpack.io");
+        loader.loadLibrary("com.github.rainchat.rcore", "inventory", "2.1", "https://jitpack.io");
+        loader.loadLibrary("com.github.rainchat.rcore", "MenuModule", "2.1", "https://jitpack.io");
+        loader.loadLibrary("com.github.rainchat.rcore", "commands", "2.1", "https://jitpack.io");
+        loader.loadLibrary("com.github.rainchat.rcore", "messages", "2.1", "https://jitpack.io");
+        loader.loadLibrary("com.github.rainchat.rcore", "storage", "2.1", "https://jitpack.io");
+        loader.loadLibrary("com.github.rainchat.rcore", "utils", "2.1", "https://jitpack.io");
+
+
+    }
 
     @Override
     public void onDisable() {
